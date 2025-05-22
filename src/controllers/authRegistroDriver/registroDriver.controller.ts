d0d11b949b48a63636a9b8099c7dbada1d3b5768
Carla Villarroel <aslecarmy@gmail.com>
2025-05-22 04:49:39 -0400

    cambios echos en el deploy:validación para asegurar selección de al menos un renter

--- src/controllers/authRegistroDriver/registroDriver.controller.ts
+++ src/controllers/authRegistroDriver/registroDriver.controller.ts
@@ -15,14 +15,14 @@ export const registrarDriverController = async (
       fecha_vencimiento,
       anversoUrl,
       reversoUrl,
-      rentersIds
+      rentersIds,
     } = req.body;
 
     console.log("📥 Datos recibidos del formulario:", req.body); // 👈 LOG 1
     console.log("👤 Usuario autenticado en req.user:", req.user); // 👈 LOG 2
 
     if (!Array.isArray(rentersIds) || rentersIds.length === 0) {
-      res.status(400).json({ message: 'Debes seleccionar al menos un renter' });
+      res.status(400).json({ message: "Debes seleccionar al menos un renter" });
       console.log("🔴 Respuesta del backend:", res.status);
       return;
     }
