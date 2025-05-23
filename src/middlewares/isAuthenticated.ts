794189257d67bb29a3855d12d01098faee29fc83
Carla Villarroel <aslecarmy@gmail.com>
2025-05-23 01:46:23 -0400

    cambios hechos en el deploy:se importa jsonwebtoken para manejo de autenticación

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,11 +1,3 @@
-/* import { Request, Response, NextFunction } from "express";
-
-export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  if (req.isAuthenticated && req.isAuthenticated()) {
-    return next();
-  }
-  return res.status(401).json({ message: "Usuario no autenticado" });
-}; */
 
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
