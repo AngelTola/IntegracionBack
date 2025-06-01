d28ab217242694343df4ac7336fe5acf30fbb5de
MaiteSuarez <suarezmaite355@gmail.com>
2025-05-31 21:25:43 -0400

    fix: se asegura autenticación antes de acceder a lista de renters

--- src/routes/visualizarRenters.routes.ts
+++ src/routes/visualizarRenters.routes.ts
@@ -4,6 +4,7 @@ import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 //Define una ruta GET en "/driver/renters"
+// Primero se ejecuta el middleware de autenticación
 router.get("/driver/renters", authMiddleware, getRentersAsignados);
 
 
